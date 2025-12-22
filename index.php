ac09bffae4691c68eaa10c73454b5489bc8a3309
Thaparkon <thapakorn1365@gmail.com>
2025-12-22 10:08:05 +0700

    add index

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>thapakorn-Blog</title>
+</head>
+<body>
+
+    <h1>thapakorn-Blog</h1>
+    <hr>
+    <p>นาย ฐปกร คำบ่อ (อาร์ตี้)</p>
+    <p>thapakorn khambo (ARTY)</p>
+    <hr>
+</body>
+</html>
